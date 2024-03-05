e2efcabce73ddee1f2224a120ed04218e0924d6b
usami-natsuki <usami-natsuki@dmm.com>
2024-03-05 10:39:16 +0900

    first commit

---
+++ src/components/block/DigHeaderNotice/DigHeaderNotice.stories.tsx
@@ -0,0 +1,17 @@
+// import type {Meta, StoryObj} from '@storybook/react';
+// import DigHeaderNotice from "./DigHeaderNotice";
+
+// const meta = {
+//   title: 'DigHeaderNotice',
+//   component: DigHeaderNotice,
+//   parameters:{
+//     layout: 'centred'
+//   },
+//   tags:['autodocs'],
+// }satisfies Meta<typeof DigHeaderNotice>;
+
+// export default meta;
+// type Story = StoryObj<typeof meta>;
+
+// export const Default: Story = {
+// };
---
+++ src/components/block/DigHeaderNotice/DigHeaderNotice.tsx
@@ -0,0 +1,15 @@
+import r2wc from "@r2wc/react-to-web-component"
+
+function DigHeaderNotice(){
+  return (
+    <div>
+      <h1>重要</h1>
+      <h1>2021年3月31日（水）月額チャンネル終了のお知らせ</h1>
+      <p>お知らせ詳細</p>
+    </div>
+  )
+}
+
+const WebDigHeaderNotice = r2wc(DigHeaderNotice)
+
+customElements.define("web-dig-header-notice", WebDigHeaderNotice)
