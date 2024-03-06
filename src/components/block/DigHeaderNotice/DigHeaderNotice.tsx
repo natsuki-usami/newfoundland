78dfad9393656ba8dd505bf063732a7a8a9ef5f8
usami-natsuki <usami-natsuki@dmm.com>
2024-03-06 15:21:26 +0900

    不要なファイル削除、config 更新

--- src/components/block/DigHeaderNotice/DigHeaderNotice.tsx
+++ src/components/block/DigHeaderNotice/DigHeaderNotice.tsx
@@ -2,8 +2,8 @@ import r2wc from "@r2wc/react-to-web-component"
 
 export default function DigHeaderNotice(){
   return (
-    <div>
-      <h1>重要</h1>
+    <div className="p-2 border border-gray-600">
+      <h1 className="text-red-600">重要</h1>
       <h1>2021年3月31日（水）月額チャンネル終了のお知らせ</h1>
       <p>お知らせ詳細</p>
     </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/js/vendor/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name].js',
+      },
+    },
+  },
+  plugins: [react(), splitVendorChunkPlugin()],
 })
